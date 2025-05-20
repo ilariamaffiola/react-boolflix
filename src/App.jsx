8c266b21824a78f18e45e954344a3c78678879bd
Ilaria <maffiolailaria1@gmail.com>
2025-05-20 12:27:33 +0200

    Add Bootstrap CSS and restructure App component layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-
+import { useState } from "react"
 
 function App() {
   
 
   return (
     <>
-      
+      <div className="container-fluid">
+        <header>
+          <div className="">
+            <div className="row justify-content-between">
+              <div className="col-3">logo</div>
+              <div className="col-4">
+                <form className="imput-group d-flex">
+                  <input 
+                  type="text"
+                  className="form-control"
+                  placeholder="Search"
+                  />
+                  <button className="btn btn-primary">Aggiungi</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </header>
+      </div>
     </>
   )
 }
