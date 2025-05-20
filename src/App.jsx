04c3357ea74641bafe990225afdd3bd98d84be0f
Ilaria <maffiolailaria1@gmail.com>
2025-05-20 13:18:32 +0200

    [UPD]Implement movie search functionality with API integration and update form handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 function App() {
-  
+  const [films, setFilms] = useState([]);
+  const [searchTitle, setSearchTitle] = useState("");
+  const filterMovies = (title) => {
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=54b9f378fd5384807b6cbb4b453b6528&query=${title}&language=it-IT`).then((res) => {
+      setFilms(res.data.results);
+    });
+  }
+
+const handleChange = (e) => {
+  setSearchTitle(e.target.value);
+}
+const handleSubmit = (e) => {
+  e.preventDefault();
+  filterMovies(searchTitle);
+  setSearchTitle("");
+}
 
   return (
     <>
@@ -11,18 +27,42 @@ function App() {
             <div className="row justify-content-between">
               <div className="col-3">logo</div>
               <div className="col-4">
-                <form className="imput-group d-flex">
+                <form className="imput-group d-flex" onSubmit={handleSubmit}>
                   <input 
                   type="text"
                   className="form-control"
                   placeholder="Search"
+                  onChange={handleChange}
+                  value={searchTitle}
                   />
-                  <button className="btn btn-primary">Aggiungi</button>
+                  <button 
+                  className="btn btn-primary"
+                  type="submit"
+                  >Aggiungi</button>
                 </form>
               </div>
             </div>
           </div>
         </header>
+        <main>
+          <div className="row">
+            {films.map((film) => (
+              <div className="col-3" key={film.id}>
+                <div className="card">
+                  <div className="card-img">
+                    <img src="" alt="" />
+                  </div>
+                  <div className="card-body">
+                    <h3>{film.title}</h3>
+                    <p>{film.original_title}</p>
+                    <p>{film.original_language}</p>
+                    <p>{film.vote_average}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </main>
       </div>
     </>
   )
